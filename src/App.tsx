13c1513e407e5e59d4d0b5baaad843b23c4d7360
Valentina Otočan <valentinaotocan@gmail.com>
2023-12-03 11:18:41 +0100

    Add route for home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from './pages/Home';
 function App() {
   return (
     <div className='max-w-[86%] mx-auto'>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
