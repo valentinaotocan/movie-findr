f3ae2a82f5812c93fd6e078aa1b5c61f3df80e9d
Valentina Otočan <valentinaotocan@gmail.com>
2023-12-03 11:27:40 +0100

    Add link to Discover button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import filmReel from "../assets/images/film-reel.jpg";
+
 function Home() {
   return (
     <div className="relative h-screen">
@@ -7,11 +9,14 @@ function Home() {
         alt="Film Reel"
         className="w-full h-full object-cover object-center"
       />
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <Link
+        to="/discover"
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      >
         <button className="bg-gray-500 text-white py-3 px-7 rounded hover:bg-yellow-500 hover:text-black">
           Discover Movies
         </button>
-      </div>
+      </Link>
     </div>
   );
 }
