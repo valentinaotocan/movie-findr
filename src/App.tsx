d18b0023a282b65165ea33c7ed5362c285035487
Valentina Otočan <valentinaotocan@gmail.com>
2023-12-08 21:43:35 +0100

    Add navbar in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Discovery from "./pages/Discovery";
+import SearchResults from "./components/SearchResults";
+import Details from "./components/Details";
+
 function App() {
   return (
     <div className="max-w-[86%] mx-auto">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/discover" element={<Discovery />} />
+        <Route path="/results" element={<SearchResults />} />
+        <Route path="/details/:movie_id" element={<Details />} />
       </Routes>
     </div>
   );
