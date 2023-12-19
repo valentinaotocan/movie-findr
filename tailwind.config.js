cc0a7bf568961b7f7e85fb0e9519b79431469a96
Valentina Otočan <valentinaotocan@gmail.com>
2023-12-19 21:59:18 +0100

    Max-width breakpoints configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2xl": { max: "1535px" },
+        // => @media (max-width: 1535px) { ... }
+
+        xl: { max: "1279px" },
+        // => @media (max-width: 1279px) { ... }
+
+        lg: { max: "1023px" },
+        // => @media (max-width: 1023px) { ... }
+
+        md: { max: "767px" },
+        // => @media (max-width: 767px) { ... }
+
+        sm: { max: "425px" },
+        // => @media (max-width: 425px) { ... }
+      },
+    },
   },
   plugins: [],
 };
