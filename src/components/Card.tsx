cd9b9f3476972508e160f0917cbc20e0f2928d35
Valentina Otočan <valentinaotocan@gmail.com>
2023-12-21 21:55:23 +0100

    Changes the Card component so that I can use it more

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,35 +10,33 @@ function Card({ movies }: CardProps) {
   const moviesWithImg = movies.filter((movie) => movie.poster_path);
 
   return (
-    <div className="overflow-x-auto flex flex-nowrap">
-      <div className="flex gap-3.5">
-        {moviesWithImg.map((movie) => (
-          <div
-            key={movie.id}
-            className="w-[190px] h-[270px] flex-shrink-0 relative"
-          >
-            <Link to={`/details/${movie.id}`}>
-              <img
-                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                alt={movie.title}
-                className="w-full h-full object-cover rounded"
-              />
-            </Link>
-            {favoritesChecker(movie) ? (
-              <FaBookmark
-                className="absolute text-[32px] -top-[2px] left-0 pl-2 text-yellow-500 cursor-pointer"
-                onClick={() => removeFromFavorites(movie)}
-              />
-            ) : (
-              <FaBookmark
-                className="absolute text-[32px] -top-[2px] left-0 pl-2 text-gray-300 cursor-pointer"
-                onClick={() => addToFavorites(movie)}
-              />
-            )}
-          </div>
-        ))}
-      </div>
-    </div>
+    <>
+      {moviesWithImg.map((movie) => (
+        <div
+          key={movie.id}
+          className="w-[11.875rem] h-[16.875rem] flex-shrink-0 relative overflow-hidden"
+        >
+          <Link to={`/details/${movie.id}`}>
+            <img
+              src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+              alt={movie.title}
+              className="w-full h-full object-cover rounded"
+            />
+          </Link>
+          {favoritesChecker(movie) ? (
+            <FaBookmark
+              className="absolute text-[1.625rem] -top-[2px] left-0 pl-2 text-yellow-400 cursor-pointer"
+              onClick={() => removeFromFavorites(movie)}
+            />
+          ) : (
+            <FaBookmark
+              className="absolute text-[1.625rem] -top-[2px] left-0 pl-2 text-gray-300 hover:text-white cursor-pointer"
+              onClick={() => addToFavorites(movie)}
+            />
+          )}
+        </div>
+      ))}
+    </>
   );
 }
 export default Card;
