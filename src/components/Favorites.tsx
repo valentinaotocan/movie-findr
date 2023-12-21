c71f4050306ac4d8dd6520e6fc9f6ba66dea4f39
Valentina Otočan <valentinaotocan@gmail.com>
2023-12-21 16:23:34 +0100

    Add responsive design for Favorites component

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -4,30 +4,30 @@ import { Link } from "react-router-dom";
 import { FaAngleDown, FaAngleUp, FaBookmark } from "react-icons/fa6";
 
 function Favorites() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
   const { favorites, favoritesChecker, removeFromFavorites } =
     useContext(FavoritesContext);
   return (
     <>
       <button
-        className={`text-white p-2 text-center inline-flex items-center ${
-          isOpen
+        className={`p-2 text-center inline-flex items-center ${
+          dropdown
             ? "bg-yellow-400 text-black"
-            : "bg-gray-500 hover:bg-yellow-400 hover:text-black"
+            : "bg-gray-500 text-white hover:bg-yellow-500 hover:text-black"
         } rounded`}
         type="button"
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={() => setDropdown(!dropdown)}
       >
         Favorites
-        {isOpen ? (
-          <FaAngleUp className="w-3 h-3 ms-2.5" />
+        {dropdown ? (
+          <FaAngleUp className="ms-2.5" />
         ) : (
-          <FaAngleDown className="w-3 h-3 ms-2.5" />
+          <FaAngleDown className="ms-2.5" />
         )}
       </button>
 
-      {isOpen && (
-        <div className="z-20 absolute rounded-lg w-44 dark:bg-gray-500 text-white mt-3 top-[52px] right-[4%]">
+      {dropdown && (
+        <div className="z-20 absolute rounded-lg w-44 dark:bg-gray-500 text-white mt-3 top-[3.25rem] right-[4%] md:right-[unset] md:top-[10.3rem] overflow-auto max-h-[31.25rem] md:max-h-[18.75rem]">
           {favorites.length > 0 ? (
             favorites.map((item) => (
               <div
@@ -36,11 +36,15 @@ function Favorites() {
               >
                 {favoritesChecker(item) && (
                   <FaBookmark
-                    className="absolute text-[26px] left-1 text-yellow-500 hover:text-opacity-70 cursor-pointer"
+                    className="absolute text-[1.625rem] left-1 text-yellow-500 hover:text-opacity-70 cursor-pointer"
                     onClick={() => removeFromFavorites(item)}
                   />
                 )}
-                <Link to={`/details/${item.id}`} className="block px-4 py-2">
+                <Link
+                  to={`/details/${item.id}`}
+                  className="block px-4 py-2"
+                  onClick={() => setDropdown(!dropdown)}
+                >
                   <p className="indent-5">{item.title}</p>
                   <p className="text-center">
                     {new Date(item.release_date).getFullYear()}
@@ -49,7 +53,7 @@ function Favorites() {
               </div>
             ))
           ) : (
-            <p className="text-yellow-500">
+            <p className="text-yellow-400 px-4 py-2">
               You don't have any favorite movies!
             </p>
           )}
